3894bcc97a4be5b66fd5bb618c583ffa9b798a59
sak89 <sascha.guida@gmail.com>
2025-05-08 20:48:03 +0200

    aggiunta dashboard

--- stanceland2025/resources/views/layouts/guest.blade.php
+++ stanceland2025/resources/views/layouts/guest.blade.php
@@ -1,31 +1,34 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'STANCELAND') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="icon" href="{{ asset('favicon.webp') }}">
+    <title>{{ config('app.name', 'STANCELAND') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="bg-background text-fontcolor flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-            <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="icon" href="{{ asset('favicon.webp') }}">
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="bg-background text-fontcolor flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+    <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
         <x-navbar />
     </header>
 
-        <div class="container mx-auto">
-            <div class="">
-                {{ $slot }}
-            </div>
+    <div class="container mx-auto">
+        <div class="">
+            {{ $slot }}
         </div>
+    </div>
 
-    </body>
-    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+
 </html>
