eea91818e28a173457d8b8de01008e1337ec5319
sak89 <sascha.guida@gmail.com>
2025-05-22 21:52:30 +0200

    delete carosello

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,31 +36,25 @@
 </section>
 
     {{-- CAROSELLO FOTO EVENTI --}}
-    <div id="default-carousel" class="relative w-full mt-20" data-carousel="slide">
-        <!-- Carousel wrapper -->
+{{--     <div id="default-carousel" class="relative w-full mt-20" data-carousel="slide">
         <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-            <!-- Item 1 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
                 <img src="/img/gallery_home/B1.jpeg" class="absolute block w-full " alt="...">
             </div>
-            <!-- Item 2 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
                 <img src="/img/gallery_home/B2.jpeg" class="absolute block w-full " alt="...">
             </div>
-            <!-- Item 3 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
                 <img src="/img/gallery_home/B3.jpeg" class="absolute block w-full" alt="...">
             </div>
-            <!-- Item 4 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
                 <img src="/img/gallery_home/B4.jpeg" class="absolute block w-full " alt="...">
             </div>
-            <!-- Item 5 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
                 <img src="/img/gallery_home/b12.jpeg" class="absolute block w-full " alt="...">
             </div>
         </div>
-        <!-- Slider indicators -->
+
         <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
             <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1"
                 data-carousel-slide-to="0"></button>
@@ -73,7 +67,7 @@
             <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5"
                 data-carousel-slide-to="4"></button>
         </div>
-        <!-- Slider controls -->
+
         <button type="button"
             class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
             data-carousel-prev>
@@ -100,7 +94,7 @@
                 <span class="sr-only">Next</span>
             </span>
         </button>
-    </div>
+    </div> --}}
 
     {{-- SEZIONE FOTO ABBIGLIAMENTO --}}
     <div class="mt-20">
